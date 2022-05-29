13a377a0710969657eb56a72310a3a5467b8e3a9
Nick Earl <nick.r.earl@gmail.com>
2022-05-29 14:25:29 -0400

    prints html file by reading command line inputs

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
+const fs = require('fs')
+
+const generatePage = require('./src/page-template')
+
 const profileDataArgs = process.argv.slice(2, process.argv.length)
 
-const printProfileData = (profileDataArr) => {
+const [name, github] = profileDataArgs
 
-    profileDataArr.forEach(profileItem => console.log(profileItem))
-}
 
-printProfileData(profileDataArgs)
+fs.writeFile('index.html', generatePage(name, github), err =>{
+    if(err) throw err
+
+    console.log('Portfolio complete! check out index.html to see the output!');
+})
